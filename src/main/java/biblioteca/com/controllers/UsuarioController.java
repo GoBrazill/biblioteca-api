262eb3041426eb337bad39b094a117786ed3b550
GoBrazill <vinicius.daraujos26@gmail.com>
2026-03-18 08:33:24 +0000

    feat: finish usuario controller

--- src/main/java/biblioteca/com/controllers/UsuarioController.java
+++ src/main/java/biblioteca/com/controllers/UsuarioController.java
@@ -1,6 +1,13 @@
 package biblioteca.com.controllers;
 
+import biblioteca.com.DTOs.UsuarioDTO;
+import biblioteca.com.entities.Usuario;
 import biblioteca.com.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 public class UsuarioController {
 
@@ -8,5 +15,27 @@ public class UsuarioController {
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("criar")
+    public ResponseEntity<?> criarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.criarUsuario(usuarioDTO));
+    }
+
+    @GetMapping("mostrar/{id}")
+    public Optional<Usuario> mostrarEmprestimoPorId(@PathVariable long id) {
+        return usuarioService.mostrarUsuarioPorID(id);
+    }
+
+    @PostMapping("atualizar/{id}")
+    public ResponseEntity<?> atualizarEmprestimo(@PathVariable long id, @Valid @RequestBody UsuarioDTO usuarioDTO) {
+
+        return ResponseEntity.ok(usuarioService.atualizarUsuario(id, usuarioDTO));
+    }
+
+    @DeleteMapping("deletar/{id}")
+    public ResponseEntity<?> deletarEmprestimoPorId(@PathVariable long id) {
+
+        return ResponseEntity.ok(usuarioService.deletarUsuario(id));
     }
 }
